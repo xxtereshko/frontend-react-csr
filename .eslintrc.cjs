d96fd3f34376ba7e43190bacba0157d7ec34cec4
Maxim Tereshko <mte@kode.ru>
2023-11-11 15:35:49 +0200

    feat(fsd): added fst structure

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
@@ -22,12 +25,81 @@ module.exports = {
       version: 'detect'
     }
   },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'perfectionist'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       {
         allowConstantExport: true
+      }
+    ],
+    'perfectionist/sort-array-includes': [
+      'warn',
+      {
+        type: 'natural',
+        order: 'asc',
+        'ignore-case': false
+      }
+    ],
+    'perfectionist/sort-enums': [
+      'warn',
+      {
+        type: 'natural',
+        order: 'asc',
+        'ignore-case': false
+      }
+    ],
+    'perfectionist/sort-exports': [
+      'warn',
+      {
+        type: 'natural',
+        order: 'asc',
+        'ignore-case': false
+      }
+    ],
+    'perfectionist/sort-imports': [
+      'warn',
+      {
+        groups: [
+          'type',
+          ['builtin', 'external'],
+          'internal-type',
+          'internal',
+          ['parent-type', 'sibling-type', 'index-type'],
+          ['parent', 'sibling', 'index'],
+          'object',
+          'unknown'
+        ],
+        'custom-groups': {
+          value: {},
+          type: {}
+        },
+        'newlines-between': 'always',
+        'internal-pattern': ['@shared/*']
+      }
+    ],
+    'perfectionist/sort-interfaces': [
+      'warn',
+      {
+        type: 'natural',
+        order: 'asc',
+        'ignore-case': false, //TODO: add onClick bottom
+        'custom-groups': {
+          top: 'id'
+        },
+        groups: ['top', 'multiline']
+      }
+    ],
+    'perfectionist/sort-jsx-props': [
+      'warn',
+      {
+        type: 'natural',
+        order: 'asc',
+        'ignore-case': false,
+        'custom-groups': {
+          callbacks: 'on*'
+        },
+        groups: ['shorthand', 'multiline', 'callbacks', 'unknown']
       }
     ]
   }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, './src/app'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@widgets': path.resolve(__dirname, './src/widgets')
+    }
+  },
+  plugins: [react()]
 })
